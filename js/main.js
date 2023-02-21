1c33781856b93b27235cf3b787d572c9190af284
kathfinder <syli@cs.washington.edu>
2023-02-21 22:07:49 +0800

    updated UI

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
 function clientConsoleLog(log_content)
 {
-    //console.log(log_content);
+    console.log(log_content);
 }
 
 class Question
 {
     constructor()
     {
-
+        //clientConsoleLog("Class instantiated");
     }
 }
 
 function ask()
 {
-    clientConsoleLog("Asked");
+    //clientConsoleLog("Asked");
 
     var i = Math.floor(Math.random() * 2);
     var input_id = "#option_" + (i + 1);
 
-    clientConsoleLog("Returning: " + i);
+    //clientConsoleLog("Returning: " + i);
 
-    document.querySelector("#answer").innerHTML = "Answer: " + document.querySelector(input_id).value;
+    document.querySelector("#answer").innerHTML = ">> Answer: " + document.querySelector(input_id).value;
+}
+
+function updateName()
+{
+    var i = Math.floor(Math.random() * 2);
+
+    //clientConsoleLog("Name option i = " + i);
+
+    document.querySelector("#salutation").innerHTML = "Dear " + (i == 0 ? "Bunny" : "Bruno") + ",";
 }
